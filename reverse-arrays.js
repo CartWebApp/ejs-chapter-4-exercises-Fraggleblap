319d3462ca42ebaf2bc33fde0dce49a045915740
Fraggleblap <Owen.Gaffney23@student.cart.org>
2024-01-31 14:27:58 -0800

    array

--- reverse-arrays.js
+++ reverse-arrays.js
@@ -1,9 +1,22 @@
 function reverseArray(arr) {
   // Add code.
+  let tempArr = [];
+  for (let i = arr.length; i > 0; i--) {
+    tempArr.push(arr[i-1]);
+  }
+  return tempArr;
 }
 
 function reverseArrayInPlace(arr) {
   // Add code.
+  let tempArr2 = [];
+  for (let i = arr.length; i > 0; i--) {
+    tempArr2.push(arr[i-1]);
+    arr.pop();
+  }
+  for (let i = 0; i < tempArr2.length; i++) {
+    arr.push(tempArr2[i]);
+  }
 }
 
 
